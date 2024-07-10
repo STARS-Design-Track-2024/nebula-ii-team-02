1bd20620ac8bf293876bf69a99b089a5ad03cfc0
Daniel <danielkimcho@gmail.com>
2024-07-10 15:00:41 +0000

    commetning

--- verilog/rtl/team_projects/team_02/t02_top.sv
+++ verilog/rtl/team_projects/team_02/t02_top.sv
@@ -5,7 +5,7 @@
 module t02_top (
 	input logic clk, nrst, enable,
   output logic [31:0] ramaddr, ramstore, 
-  output logic Ren, Wen, 
+  //output logic Ren, Wen, 
   input logic [31:0] ramload, start_addr,
   input logic busy_o
   // output logic lcd_en, lcd_rw, lcd_rs,
@@ -25,25 +25,25 @@ module t02_top (
 
 
 
-t02_mux aluMux(.in1(immOut), .in2(regData2), .en(aluSrc), .out(aluIn));
+//t02_mux aluMux(.in1(immOut), .in2(regData2), .en(aluSrc), .out(aluIn));
 
-t02_alu arith(.aluOP(aluOP), .inputA(regData1), .inputB(aluIn), .ALUResult(aluOut), .zero(zero), .negative(negative));
+//t02_alu arith(.aluOP(aluOP), .inputA(regData1), .inputB(aluIn), .ALUResult(aluOut), .zero(zero), .negative(negative));
 
-t02_register_file DUT(.clk(clk), .nRST(nrst), .reg_write(regWrite), .read_index1(regsel1), .read_index2(regsel2), 
-.read_data1(regData1), .read_data2(regData2), .write_index(w_reg), .write_data(writeData));
+//t02_register_file DUT(.clk(clk), .nRST(nrst), .reg_write(regWrite), .read_index1(regsel1), .read_index2(regsel2), 
+//.read_data1(regData1), .read_data2(regData2), .write_index(w_reg), .write_data(writeData));
 
-t02_control controller (.cuOP(cuOP), .instruction(instruction), 
-.reg_1(regsel1), .reg_2(regsel2), .rd(w_reg),
-.imm(imm), .aluOP(aluOP), .regWrite(regWrite), .memWrite(memWrite), .memRead(memRead), .aluSrc(aluSrc));
+//t02_control controller (.cuOP(cuOP), .instruction(instruction), 
+//.reg_1(regsel1), .reg_2(regsel2), .rd(w_reg),
+//.imm(imm), .aluOP(aluOP), .regWrite(regWrite), .memWrite(memWrite), .memRead(memRead), .aluSrc(aluSrc));
 
-t02_pc testpc(.clk(clk), .nRST(nrst), .ALUneg(negative), .Zero(zero), .iready(i_ready), .PCaddr(pc), .cuOP(cuOP), .rs1Read(regData1), .signExtend(immOut), .enable(enable), .start_addr(start_addr));
+//t02_pc testpc(.clk(clk), .nRST(nrst), .ALUneg(negative), .Zero(zero), .iready(i_ready), .PCaddr(pc), .cuOP(cuOP), .rs1Read(regData1), .signExtend(immOut), .enable(enable), .start_addr(start_addr));
 
-t02_writeToReg write(.cuOP(cuOP), .memload(memload), .aluOut(aluOut), .imm(immOut), .pc(pc), .writeData(writeData), .negative(negative));
+//t02_writeToReg write(.cuOP(cuOP), .memload(memload), .aluOut(aluOut), .imm(immOut), .pc(pc), .writeData(writeData), .negative(negative));
 
-t02_signExtender signex(.imm(imm), .immOut(immOut), .CUOp(cuOP));
+//t02_signExtender signex(.imm(imm), .immOut(immOut), .CUOp(cuOP));
 
-t02_request ru(.CLK(clk), .nRST(nrst), .imemload(instruction), .imemaddr(pc), .dmmaddr(aluOut), .dmmstore(regData2), .ramaddr(ramaddr), .ramload(ramload), .ramstore(ramstore), 
-.cuOP(cuOP), .Wen(Wen), .busy_o(busy_o), .dmmload(memload), .i_ready(i_ready), .d_ready(d_ready),.Ren(Ren), .en(enable));
+//t02_request ru(.CLK(clk), .nRST(nrst), .imemload(instruction), .imemaddr(pc), .dmmaddr(aluOut), .dmmstore(regData2), .ramaddr(ramaddr), .ramload(ramload), .ramstore(ramstore), 
+//.cuOP(cuOP), .Wen(Wen), .busy_o(busy_o), .dmmload(memload), .i_ready(i_ready), .d_ready(d_ready),.Ren(Ren), .en(enable));
 
 // t02_edgeDetector edg2(.clk(clk), .nRst_i(nrst), .button_i(~keyStrobe), .button_p(enData));
 // t02_keypad pad (.clk(clk), .rst(nrst), .receive_ready(keyStrobe), .data_received(halfData), .read_row(read_row), .scan_col(scan_col));
